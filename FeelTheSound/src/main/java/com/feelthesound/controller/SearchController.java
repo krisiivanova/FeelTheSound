54064494dff92b4db754f2a86bae4f2a1ae4f385
krisiivanova <kristinagivanova1995@abv.bg>
2016-10-16 12:13:15 +0300

    method for showing songs by searchtext added in the search controlller

--- FeelTheSound/src/main/java/com/feelthesound/controller/SearchController.java
+++ FeelTheSound/src/main/java/com/feelthesound/controller/SearchController.java
@@ -1,14 +1,44 @@
 package com.feelthesound.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.feelthesound.model.ISong;
+import com.feelthesound.model.DAOs.SongDAO;
 
 @Controller
 public class SearchController {
 
+	@Autowired
+	SongDAO songDao;
+
 	@RequestMapping(value = "/Search", method = RequestMethod.GET)
 	public String searchPage() {
 		return "search";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/text")
+	public ModelAndView getSubView(@RequestParam("text") String text, Model model, HttpSession session) {
+		ModelAndView modelAndView = null;
+
+		try {
+			modelAndView = new ModelAndView("searchListNotLogged");
+			List<ISong> songList = songDao.getSongsBySearchText(text);
+			modelAndView.addObject("songs", songList);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return modelAndView;
 	}
 }
